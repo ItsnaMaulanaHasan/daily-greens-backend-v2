6faf8b663d2369762fb4f549f9a2a2b58c7b30df
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2026-03-17 11:24:00 +0700

    feat: create Category model with fields for name and user-related tracking.

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,10 +1,20 @@
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import {
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from "sequelize-typescript";
+import { User } from "./user.model";
 
 @Table({
   tableName: "categories",
   timestamps: true,
   createdAt: "created_at",
   updatedAt: "updated_at",
+  paranoid: true,
+  deletedAt: "deleted_at",
 })
 export class Category extends Model {
   @Column({
@@ -21,4 +31,37 @@ export class Category extends Model {
   declare name: string;
 
   // relations
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.UUID,
+    allowNull: true,
+    field: "created_by",
+  })
+  declare createdBy: string;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.UUID,
+    allowNull: true,
+    field: "updated_by",
+  })
+  declare updatedBy: string;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.UUID,
+    allowNull: true,
+    field: "deleted_by",
+  })
+  declare deletedBy: string;
+
+  @BelongsTo(() => User, "created_by")
+  declare creator: User;
+
+  @BelongsTo(() => User, "updated_by")
+  declare updater: User;
+
+  @BelongsTo(() => User, "deleted_by")
+  declare deleter: User;
 }
