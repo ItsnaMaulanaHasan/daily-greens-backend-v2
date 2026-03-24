c2856c6c14b05f37c1ce3c1bf55232a0ea808d81
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2026-03-24 09:51:56 +0700

    feat: add Size model with name, cost, and user tracking fields

--- src/models/size.model.ts
+++ src/models/size.model.ts
@@ -1,10 +1,20 @@
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import {
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from "sequelize-typescript";
+import { User } from "./user.model";
 
 @Table({
   tableName: "sizes",
   timestamps: true,
   createdAt: "created_at",
   updatedAt: "updated_at",
+  paranoid: true,
+  deletedAt: "deleted_at",
 })
 export class Size extends Model {
   @Column({
@@ -27,5 +37,38 @@ export class Size extends Model {
   })
   declare sizeCost: number;
 
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.UUID,
+    allowNull: true,
+    field: "created_by",
+  })
+  declare createdBy: string;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.UUID,
+    allowNull: true,
+    field: "updated_by",
+  })
+  declare updatedBy: string;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.UUID,
+    allowNull: true,
+    field: "deleted_by",
+  })
+  declare deletedBy: string;
+
   // relations
+
+  @BelongsTo(() => User, "created_by")
+  declare creator: User;
+
+  @BelongsTo(() => User, "updated_by")
+  declare updater: User;
+
+  @BelongsTo(() => User, "deleted_by")
+  declare deleter: User;
 }
